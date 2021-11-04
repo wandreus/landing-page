3709a1e9ffc0cfc74e42e4ee89721e5e21720149
Wandreus Pereira <wandreus.pereira@outlook.com>
2021-11-04 03:19:29 +0000

    redirect linkedin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,14 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
-import SectionHero from 'components/SectionHero'
-import SectionAboutProject from 'components/SectionAboutProject'
-import SectionTech from 'components/SectionTech'
-import SectionConcepts from 'components/SectionConcepts'
-import SectionModules from 'components/SectionModules'
-import SectionAgenda from 'components/SectionAgenda'
-import PricingBox from 'components/PricingBox'
-import SectionAboutUs from 'components/SectionAboutUs'
-import SectionReviews from 'components/SectionReviews'
-import SectionFaq from 'components/SectionFaq'
-import Footer from 'components/Footer'
-import JsonSchema from 'components/JsonSchema'
+const Index = () => {
+  useEffect(() => {
+    window.location = 'https://www.linkedin.com/in/wandreus'
+  }, [])
 
-const Index = () => (
-  <>
-    <SectionHero />
-    <SectionAboutProject />
-    <SectionTech />
-    <SectionConcepts />
-    <SectionModules />
-    <SectionAgenda />
-    <PricingBox />
-    <SectionAboutUs />
-    <SectionReviews />
-    <SectionFaq />
-    <Footer />
-    <JsonSchema />
-  </>
-)
+  return (
+    <>
+    </>
+  )
+}
 
 export default Index
